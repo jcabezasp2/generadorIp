eed47bc01a3f9af871d8c1c93c49bb4362108dfc
Javier Cabezas <javiercp69@educastur.es>
2022-03-25 13:19:21 +0100

    Problema resuelto

--- src/generadorIp/GeneradorIp.java
+++ src/generadorIp/GeneradorIp.java
@@ -1,5 +1,32 @@
 package generadorIp;
 
+import java.util.Random;
+
 public class GeneradorIp {
 
+	Random random;
+	
+	public int generarNumero(int min, int max) {
+		
+		
+		
+		
+		return random.nextInt(max + 1) + min;
+	}
+	
+	public String generarIp() {
+		StringBuilder sb = new StringBuilder();
+		
+		
+			sb.append(generarNumero(1, 254));
+			sb.append(".");
+			sb.append(generarNumero(0, 254));
+			sb.append(".");
+			sb.append(generarNumero(0, 254));
+			sb.append(".");
+			sb.append(generarNumero(1, 254));
+		
+		
+		return sb.toString();
+	}
 }
